0917a3fd7c9509742a41a76665e235aa32a56ad0
“Reo <leo.0822.kaz@gmail.com>
2021-06-08 23:54:03 +0900

    わかりません

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
 
+    def index
+        @post = Post.all
+    end
+
     def new
         @post = Post.new
     end
+
+    def create
+        post = Post.new(post_params)
+        if post.save
+          redirect_to :action => "index"
+        else
+          redirect_to :action => "new"
+        end
+      end
+    
+      private
+      def post_params
+        params.require(:post).permit(:title)
+      end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get 'post/index' => 'posts#index'
+
   get 'posts/new' => 'posts#new'
 
-  post 'posts' => posts#create'
+  post 'post' => 'posts#create'
+
 end
